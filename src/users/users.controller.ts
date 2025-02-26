532cb447c7912d9e37e6e6da6a211ed291163b9e
Daniel Carreon <daniel.carreong@gmail.com>
2025-02-25 18:20:32 -0600

    - Adding vercel.json to be able to deploy to Vercel - Changes in exception handling for user controller - Adding Prisma management with Supabase

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,11 @@ import {
   Delete,
   Get,
   Logger,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, UserDto } from './dto/user.dto';
@@ -18,28 +20,59 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
+  create(@Body() createUserDto: CreateUserDto): Promise<UserDto> | null {
     this.logger.debug(createUserDto);
-    return this.usersService.create(createUserDto);
+    try {
+      return this.usersService.create(createUserDto);
+    } catch (error) {
+      this.logger.error(error);
+      throw new NotFoundException(
+        `Error creating user with username ${createUserDto.username}`,
+      );
+    }
   }
 
   @Get()
-  findAll(): Promise<UserDto[]> {
-    return this.usersService.findAll();
+  findAll(): Promise<UserDto[]> | null {
+    try {
+      return this.usersService.findAll();
+    } catch (error) {
+      this.logger.error(error);
+      return null;
+    }
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+    try {
+      return this.usersService.findOne(id);
+    } catch (error) {
+      this.logger.error(error);
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UserDto) {
-    return this.usersService.update(id, updateUserDto);
+    try {
+      return this.usersService.update(id, updateUserDto);
+    } catch (error) {
+      this.logger.error(error);
+      throw new UnprocessableEntityException(
+        `Error updating user with id ${id}`,
+      );
+    }
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+    try {
+      return this.usersService.remove(id);
+    } catch (error) {
+      this.logger.error(error);
+      throw new UnprocessableEntityException(
+        `Error deleting user with id ${id}`,
+      );
+    }
   }
 }
